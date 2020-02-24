161bc7c599c09d458ba10a8cbf69711e2523fa4c
Shannon Scirghio <shanscirg7@gmail.com>
2020-02-23 21:17:07 -0700

    need to complete store.js file

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const store = require("../db/store");
 
-// GET "/api/notes" responds with all notes from the database
 router.get("/notes", function(req, res) {
   store
     .getNotes()
@@ -16,7 +15,6 @@ router.post("/notes", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
-// DELETE "/api/notes" deletes the note with an id equal to req.params.id
 router.delete("/notes/:id", function(req, res) {
   store
     .removeNote(req.params.id)
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,12 +1,10 @@
 var path = require("path");
 var router = require("express").Router();
 
-// "/notes" responds with the notes.html file
 router.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-// All other routes respond with the index.html file
 router.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
